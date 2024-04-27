c216da26e8ee01575dfdd3a8bb9b84e0c84c766a
Mohamed Shakin <m.shakin2005@gmail.com>
2024-04-27 22:33:50 +0400

    Create script2.js

---
+++ JavaScript/script2.js
@@ -0,0 +1,104 @@
+var button1=document.querySelector("#button1")
+var button2=document.querySelector("#button2")
+var button3=document.querySelector("#button3")
+var button4=document.querySelector("#button4")
+var button5=document.querySelector("#button5")
+var button6=document.querySelector("#button6")
+
+button1.addEventListener("click", function(){
+    answer1=document.querySelector("#answer1")
+    if (answer1.style.display === "block") {
+        answer1.style.display = "none";
+        button1.style.transform= "rotate(0deg)"
+        button1.style.transition= "2s"
+
+}  
+    else { (answer1.style.display === "none") 
+        answer1.style.display="block";
+        button1.style.transform= "rotate(45deg)"
+        button1.style.transition= "2s"
+    }
+}
+)
+
+button2.addEventListener("click", function(){
+    answer2=document.querySelector("#answer2")
+    if (answer2.style.display === "block") {
+        answer2.style.display = "none";
+        button2.style.transform= "rotate(0deg)"
+        button2.style.transition= "2s"
+
+}  
+    else { (answer2.style.display === "none") 
+        answer2.style.display="block";
+        button2.style.transform= "rotate(45deg)"
+        button2.style.transition= "2s"
+    }
+}
+)
+
+button3.addEventListener("click", function(){
+    answer3=document.querySelector("#answer3")
+    if (answer3.style.display === "block") {
+        answer3.style.display = "none";
+        button3.style.transform= "rotate(0deg)"
+        button3.style.transition= "2s"
+
+}  
+    else { (answer3.style.display === "none") 
+        answer3.style.display="block";
+        button3.style.transform= "rotate(45deg)"
+        button3.style.transition= "2s"
+    }
+}
+)
+
+button4.addEventListener("click", function(){
+    answer4=document.querySelector("#answer4")
+    if (answer4.style.display === "block") {
+        answer4.style.display = "none";
+        button4.style.transform= "rotate(0deg)"
+        button4.style.transition= "2s"
+
+}  
+    else { (answer4.style.display === "none") 
+        answer4.style.display="block";
+        button4.style.transform= "rotate(45deg)"
+        button4.style.transition= "2s"
+    }
+}
+)
+
+button5.addEventListener("click", function(){
+    answer5=document.querySelector("#answer5")
+    if (answer5.style.display === "block") {
+        answer5.style.display = "none";
+        button5.style.transform= "rotate(0deg)"
+        button5.style.transition= "2s"
+
+}  
+    else { (answer5.style.display === "none") 
+        answer5.style.display="block";
+        button5.style.transform= "rotate(45deg)"
+        button5.style.transition= "2s"
+    }
+}
+)
+
+button6.addEventListener("click", function(){
+    answer=document.querySelector("#answer6")
+    if (answer6.style.display === "block") {
+        answer6.style.display = "none";
+        button6.style.transform= "rotate(0deg)"
+        button6.style.transition= "2s"
+
+}  
+    else { (answer6.style.display === "none") 
+        answer6.style.display="block";
+        button6.style.transform= "rotate(45deg)"
+        button6.style.transition= "2s"
+    }
+}
+)
+
+
