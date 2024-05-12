63eda5a6e441c2c9b1233d2ac40c243f5fc93a23
Mohamed Shakin <m.shakin2005@gmail.com>
2024-05-12 16:33:04 +0400

    Update script2.js

--- JavaScript/script2.js
+++ JavaScript/script2.js
@@ -1,18 +1,19 @@
-var button1=document.querySelector("#button1")
-var button2=document.querySelector("#button2")
-var button3=document.querySelector("#button3")
-var button4=document.querySelector("#button4")
-var button5=document.querySelector("#button5")
-var button6=document.querySelector("#button6")
+var button1=document.querySelector("#button1")    //It gets element of #button1 from document
+var button2=document.querySelector("#button2")    //It gets element of #button2 from document
+var button3=document.querySelector("#button3")    //It gets element of #button3 from document
+var button4=document.querySelector("#button4")    //It gets element of #button4 from document
+var button5=document.querySelector("#button5")    //It gets element of #button5 from document
+var button6=document.querySelector("#button6")    //It gets element of #button6 from document
 
 button1.addEventListener("click", function(){
-    answer1=document.querySelector("#answer1")
+    answer1=document.querySelector("#answer1")   //It gets element of #answer1 from document
+    //When button is clicked if the answer is already appears then answer will dissapear and the button turns back to 0 degree and forms "+" button in 2 seconds.
     if (answer1.style.display === "block") {
         answer1.style.display = "none";
         button1.style.transform= "rotate(0deg)"
         button1.style.transition= "2s"
-
 }  
+    //When button is clicked if answer is hidden then answer will appear and the button turns into 45 degree and forms "x" button in 2 seconds.
     else { (answer1.style.display === "none") 
         answer1.style.display="block";
         button1.style.transform= "rotate(45deg)"
@@ -22,13 +23,15 @@ button1.addEventListener("click", function(){
 )
 
 button2.addEventListener("click", function(){
-    answer2=document.querySelector("#answer2")
+    answer2=document.querySelector("#answer2")  //It gets element of #answer1 from document
+    //When button is clicked if the answer is already appears then answer will dissapear and the button turns back to 0 degree and forms "+" button in 2 seconds.
     if (answer2.style.display === "block") {
         answer2.style.display = "none";
         button2.style.transform= "rotate(0deg)"
         button2.style.transition= "2s"
 
 }  
+    //When button is clicked if answer is hidden then answer will appear and the button turns into 45 degree and forms "x" button in 2 seconds.
     else { (answer2.style.display === "none") 
         answer2.style.display="block";
         button2.style.transform= "rotate(45deg)"
@@ -38,13 +41,15 @@ button2.addEventListener("click", function(){
 )
 
 button3.addEventListener("click", function(){
-    answer3=document.querySelector("#answer3")
+    answer3=document.querySelector("#answer3")  //It gets element of #answer1 from document
+    //When button is clicked if the answer is already appears then answer will dissapear and the button turns back to 0 degree and forms "+" button in 2 seconds.
     if (answer3.style.display === "block") {
         answer3.style.display = "none";
         button3.style.transform= "rotate(0deg)"
         button3.style.transition= "2s"
 
 }  
+    //When button is clicked if answer is hidden then answer will appear and the button turns into 45 degree and forms "x" button in 2 seconds.
     else { (answer3.style.display === "none") 
         answer3.style.display="block";
         button3.style.transform= "rotate(45deg)"
@@ -54,13 +59,15 @@ button3.addEventListener("click", function(){
 )
 
 button4.addEventListener("click", function(){
-    answer4=document.querySelector("#answer4")
+    answer4=document.querySelector("#answer4")  //It gets element of #answer1 from document
+    //When button is clicked if the answer is already appears then answer will dissapear and the button turns back to 0 degree and forms "+" button in 2 seconds.
     if (answer4.style.display === "block") {
         answer4.style.display = "none";
         button4.style.transform= "rotate(0deg)"
         button4.style.transition= "2s"
 
 }  
+    //When button is clicked if answer is hidden then answer will appear and the button turns into 45 degree and forms "x" button in 2 seconds.
     else { (answer4.style.display === "none") 
         answer4.style.display="block";
         button4.style.transform= "rotate(45deg)"
@@ -70,13 +77,15 @@ button4.addEventListener("click", function(){
 )
 
 button5.addEventListener("click", function(){
-    answer5=document.querySelector("#answer5")
+    answer5=document.querySelector("#answer5")  //It gets element of #answer1 from document
+    //When button is clicked if the answer is already appears then answer will dissapear and the button turns back to 0 degree and forms "+" button in 2 seconds.
     if (answer5.style.display === "block") {
         answer5.style.display = "none";
         button5.style.transform= "rotate(0deg)"
         button5.style.transition= "2s"
 
 }  
+    //When button is clicked if answer is hidden then answer will appear and the button turns into 45 degree and forms "x" button in 2 seconds.
     else { (answer5.style.display === "none") 
         answer5.style.display="block";
         button5.style.transform= "rotate(45deg)"
@@ -86,13 +95,15 @@ button5.addEventListener("click", function(){
 )
 
 button6.addEventListener("click", function(){
-    answer=document.querySelector("#answer6")
+    answer=document.querySelector("#answer6")  //It gets element of #answer1 from document
+    //When button is clicked if the answer is already appears then answer will dissapear and the button turns back to 0 degree and forms "+" button in 2 seconds.
     if (answer6.style.display === "block") {
         answer6.style.display = "none";
         button6.style.transform= "rotate(0deg)"
         button6.style.transition= "2s"
 
 }  
+    //When button is clicked if answer is hidden then answer will appear and the button turns into 45 degree and forms "x" button in 2 seconds.
     else { (answer6.style.display === "none") 
         answer6.style.display="block";
         button6.style.transform= "rotate(45deg)"
